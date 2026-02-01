60d3bd2ec7a6bebff181eeaf885caad3c3d75d09
Renan Caliga <rcaliga@gmail.com>
2026-02-01 12:50:47 -0300

    finaliza endpoint `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "valor" });
+  const updatedAt = new Date().toISOString();
+
+  const dbServerVersionResult = await database.query("SHOW server_version;");
+  const dbServerVersionValue = dbServerVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnectionsValue = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const datadbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbServerVersionValue,
+        max_connections: parseInt(dbMaxConnectionsValue),
+        opened_connections: datadbOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
